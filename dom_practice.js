451ad6640a5732778e16274bf70cb69d945be325
monicaleep <monica.parrillo@gmail.com>
2020-09-05 17:43:00 -0500

    Get h2 element, save it and log it

--- dom_practice.js
+++ dom_practice.js
@@ -1,2 +1,7 @@
+// Get the element with the id you assigned and save it to a variable . Console.log that variable.
 const myId = document.querySelector('#header')
 console.log(myId)
+
+// Get the h2 element and save it to a variable h2. Console.log that variable.
+const myHTwo = document.querySelector('h2')
+console.log(myHTwo)
